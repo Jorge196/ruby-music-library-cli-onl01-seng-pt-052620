c04be475cd98958fe6b1a5f084c88664db9e46d5
Jorge Ramirez <joramirez96@gmail.com>
2020-07-06 19:36:39 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,3 +1,13 @@
 class MusicImporter
-  
+  attr_accessor :path, :song, :artist, :genre, :MusicLibraryController
+
+  def files
+    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end
 end
